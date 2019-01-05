ca9cea4b5bf06f57fd7e4136e910b14bc5b10a4b
robcronin <robc@theodo.co.uk>
2019-01-05 15:13:24 +0000

    Fix bad requires in source-map-support (WIP)

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,37 @@
 
 echo 'ℹ️  Transforming files with babel ℹ️'
 rm -rf deploy-temp
+rm -rf src/temp
 mkdir deploy-temp
+yarn install --modules-folder src/temp --production=true --ignore "package.json"
+yarn
+cd src
+mv temp node_modules
+cd ..
 yarn babel src --out-dir deploy-temp/LAYER_NAME
 echo '✅  Transform complete ✅'
 
-echo 'ℹ️  Installing yarn dependencies ℹ️'
-yarn install --modules-folder deploy-temp/node_modules --production=true
-echo '✅  Installed yarn dependencies ✅'
+echo 'ℹ️  Fixing bad requires in source-map-support (WIP) ℹ️'
+cd deploy-temp/LAYER_NAME/node_modules/source-map-support
+sed -i.bak "s/require('.\/')/require('.\/source-map-support')/" register.js && rm register.js.bak
+sed -i.bak "s/require('source-map')/require('.\/node_modules\/source-map\/source-map')/" source-map-support.js && rm source-map-support.js.bak
+cd ../../../..
+echo '✅  Fixed bad requires in source-map-support (WIP) ✅'
 
 echo 'ℹ️  Zipping files ℹ️'
 cd deploy-temp
 zip -r LAYER_NAME.zip LAYER_NAME
+cd ..
 echo '✅  Files zipped ✅'
  
 echo 'ℹ️  Creating Serverless package ℹ️'
-cd ..
 sls package
 mv deploy-temp/LAYER_NAME.zip .serverless/LAYER_NAME.zip
 echo '✅  Serverless package created ✅'
 
 echo 'ℹ️  Cleaning up ℹ️'
 rm -rf deploy-temp
+rm -rf src/temp
 echo '✅  Cleaned up ✅'
 
 echo 'ℹ️  Deploying ℹ️'
--- local.sh
+++ local.sh
@@ -2,13 +2,23 @@
 
 echo 'ℹ️  Transforming files with babel ℹ️'
 rm -rf local-temp
+rm -rf src/temp
 mkdir local-temp
+yarn install --modules-folder src/temp --production=true --ignore "package.json"
+yarn
+cd src
+mv temp node_modules
+cd ..
 yarn babel src --out-dir local-temp/LAYER_NAME
 echo '✅  Transform complete ✅'
 
-echo 'ℹ️  Installing yarn dependencies ℹ️'
-yarn install --modules-folder deploy-temp/node_modules --production=true
-echo '✅  Installed yarn dependencies ✅'
+echo 'ℹ️  Fixing bad requires in source-map-support (WIP) ℹ️'
+cd local-temp/LAYER_NAME/node_modules/source-map-support
+sed -i.bak "s/require('.\/')/require('.\/source-map-support')/" register.js && rm register.js.bak
+sed -i.bak "s/require('source-map')/require('.\/node_modules\/source-map\/source-map')/" source-map-support.js && rm source-map-support.js.bak
+cd ../../../..
+echo '✅  Fixed bad requires in source-map-support (WIP) ✅'
+
 
 echo 'ℹ️  Copying files to /opt ℹ️'
 sudo cp -r local-temp/LAYER_NAME /opt
@@ -17,4 +27,5 @@ echo '✅  Copied fules to /opt ✅'
 
 echo 'ℹ️  Cleaning up ℹ️'
 rm -rf local-temp
+rm -rf src/temp
 echo '✅  Cleaned up ✅'
